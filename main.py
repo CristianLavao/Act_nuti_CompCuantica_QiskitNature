5bc96a407def6d61e0482100e5211cd572c7cc4c
CristianLavao <camilolavao14@gmail.com>
2023-03-21 22:03:26 -0500

    add: python code

---
+++ main.py
@@ -0,0 +1,48 @@
+from qiskit_nature.units import DistanceUnit
+from qiskit_nature.second_q.drivers import PySCFDriver
+
+from qiskit_nature.second_q.algorithms import GroundStateEigensolver, NumPyMinimumEigensolverFactory
+from qiskit_nature.second_q.mappers import JordanWignerMapper, QubitConverter
+
+driver = PySCFDriver(
+    atom="H 0 0 0; H 0 0 0.735",
+    basis="sto3g",
+    charge=0,
+    spin=0,
+    unit=DistanceUnit.ANGSTROM,
+)
+
+problem = driver.run()
+print(problem)
+    
+
+hamiltonian = problem.hamiltonian
+
+coefficients = hamiltonian.electronic_integrals
+print(coefficients.alpha)
+
+
+second_q_op = hamiltonian.second_q_op()
+print(second_q_op)
+
+repulsion_energy = hamiltonian.nuclear_repulsion_energy
+print(repulsion_energy)
+
+print(problem.molecule)
+
+print(problem.reference_energy)
+
+print(problem.num_particles)
+
+print(problem.num_spatial_orbitals)
+
+print(problem.basis)
+
+
+solver = GroundStateEigensolver(
+    QubitConverter(JordanWignerMapper()),
+    NumPyMinimumEigensolverFactory(),
+)
+
+result = solver.solve(problem)
+print(result)
